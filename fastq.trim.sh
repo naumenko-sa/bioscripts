3ba33b3d65a741d0f4073472f8cf44f767feb4ea
naumenko-sa <sergey_bulk@list.ru>
2015-12-10 11:02:43 -0500

    new file:   fastq.qc.2files.pbs 	new file:   fastq.qc.2files.sh modified:   fastq.qc.pbs 	modified:   fastq.qc.sh 	modified:   fastq.split.sh 	modified:   fastq.trim.pbs 	modified:   fastq.trim.sh 	new file:   go.star1.pbs 	modified:   qhost

--- fastq.trim.sh
+++ fastq.trim.sh
@@ -7,14 +7,14 @@ then
     exit 1
 fi
 
-java -jar /home/tools/trimmomatic/trimmomatic-0.30.jar PE -threads $6 -phred33 \
+java -jar /hpf/tools/centos6/trimmomatic/0.32/trimmomatic-0.32.jar PE -threads $6 -phred33 \
   $1 $2 \
   `echo $1 | sed s/fq/trim.fq/` forward_unpaired.fq `echo $2 | sed s/fq/trim.fq/` reverse_unpaired.fq \
   ILLUMINACLIP:$3:2:40:15 LEADING:$4 TRAILING:$4 SLIDINGWINDOW:4:$4 MINLEN:$5
 
-nreads.sh `echo $1 | sed s/fq/trim.fq/`
+fastq.nreads.sh `echo $1 | sed s/fq/trim.fq/`
 
 cat forward_unpaired.fq reverse_unpaired.fq > unpaired.fq
-nreads.sh unpaired.fq
+fastq.nreads.sh unpaired.fq
 
 rm forward_unpaired.fq reverse_unpaired.fq
