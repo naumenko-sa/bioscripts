53ebf68e95ec5a42a89a24a6a0a0be4feb0447af
naumenko-sa <evolgenomicslab@gmail.com>
2016-02-03 16:59:52 -0500

    new file:   go.dnaseq.pbs 	modified:   go.golipour.pbs 	new file:   go.golipour1.pbs 	new file:   vcf.get_context.sh 	new file:   vcf2spectrum.sh 	deleted:    .nreads 	deleted:    alignment.count_nongappy_columns.sh 	deleted:    go.gatk.all.pbs 	deleted:    go.gatk2.pbs.o2609779 	deleted:    plot_diagram.R

---
+++ vcf.get_context.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read line;
+do
+    read chr pos ref alt <<< "$line";
+    triplet=`tail -n1  chr${chr}.fasta | awk -v c=$pos '{print substr($0,c-1,3)}'`;
+    echo $chr $pos $ref $alt $triplet;
+done < $1;
