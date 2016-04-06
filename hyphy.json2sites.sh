16c5c98768a4ea3d14de599e16ec42bc17c443af
naumenko-sa <sergey_bulk@list.ru>
2016-04-05 23:47:23 -0400

    modified:   hyphy.json2sites.sh

--- hyphy.json2sites.sh
+++ hyphy.json2sites.sh
@@ -3,7 +3,7 @@
 # extracts number of sites under selection from json output of resultsBusted
 # we are looking for evidence rations section, then optimized null
 # these values are for each site without gaps
-# we return if (log2(X) > 10) print 1; else print 0;
+# we return if (2*log2(X) >= 9) print 1; else print 0;
 # if site is under selection we print 1 otherwise we print 0;
 # finally we print only numbers of sites with positive selection
 
@@ -11,4 +11,4 @@
 
 # usage hyphy.json2sites.sh file.json
 
-cat $1  | awk 'BEGIN{flag=0;}{if ($0 ~ /evidence ratios/) flag=1; if (($0 ~ /optimized null/) && (flag==1)) {getline;print $0;}}' | sed s/","/"\n"/g | sed s/"\["// | sed s/"\]"// | awk '{print log($0)/log(2)}' | awk '{if ($0>=10) print 1;else print 0;}' | awk '{if ($0 == 1) print NR}'
+cat $1  | awk 'BEGIN{flag=0;}{if ($0 ~ /evidence ratios/) flag=1; if (($0 ~ /optimized null/) && (flag==1)) {getline;print $0;}}' | sed s/","/"\n"/g | sed s/"\["// | sed s/"\]"// | awk '{print log($0)/log(2)}' | awk '{if (2*$0>=9) print 1;else print 0;}' | awk '{if ($0 == 1) print NR}'
