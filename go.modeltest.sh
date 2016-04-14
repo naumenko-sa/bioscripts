ae625ffe4b927b23fa3e78d32d966f492906fb9c
naumenko-sa <evolgenomicslab@gmail.com>
2016-04-14 22:37:10 +0300

    modified:   go.blastp_xml.pbs 	modified:   go.modeltest.sh 	modified:   go.raxml.sh 	modified:   go.raxml_boot.pbs

--- go.modeltest.sh
+++ go.modeltest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #go.modeltest.sh file.fasta num_threads
 
-java -jar /mnt/lustre/tools/jmodeltest2/dist/jModelTest.jar -d $1 -s 11 -i -g 4 -f -BIC -a -tr $2 -o $1.modeltest
+java -jar /mnt/lustre/tools/jmodeltest2/dist/jModelTest.jar -d $1 -s 3 -g 4 -f -BIC -a -tr $2 -o $1.modeltest
+#-s 11 -i
