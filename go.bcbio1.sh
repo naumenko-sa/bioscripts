e340a6fe2a54b8f12ed462823e5aafbdfcd2761b
naumenko-sa <sergey_bulk@list.ru>
2016-04-25 10:53:48 -0400

    modified:   bcbio.exome.template.yaml 	modified:   go.bcbio1.sh 	new file:   kristin.process.sh 	new file:   kristin.process1.sh 	new file:   remap.sed 	deleted:    .nreads 	deleted:    alignment.count_nongappy_columns.sh 	deleted:    go.gatk.all.pbs 	deleted:    go.gatk2.pbs.o2609779 	deleted:    plot_diagram.R

--- go.bcbio1.sh
+++ go.bcbio1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#PBS -l walltime=50:00:00,nodes=1:ppn=16
+#PBS -l walltime=100:00:00,nodes=1:ppn=16
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=120g
 
 echo "START: " `date`;
 
-bcbio_nextgen.py ../config/project.yaml -n 16
+bcbio_nextgen.py ../config/${project}.yaml -n 16
 
 echo "END: " `date`;
 
---
+++ kristin.process1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d /home/naumenko/work
+#PBS -l vmem=20g
+
+module load p7zip/15.14.1
+
+FDIR=/hpf/largeprojects/ccmbio/boycott_exome/raw
+
+echo "Start " `date`
+
+mkdir /scratch/${file}_dir
+cp $FDIR/$file /scratch/${file}_dir
+
+echo "Copy done " `date`
+
+cd /scratch/${file}_dir
+7za e -bd $file
+
+
+echo "Unpacking done" `date`
+
+for f in *.gz;do gunzip $f;done;
+
+cat *R1*> $file.r1.fq
+cat *R2*> $file.r2.fq
+
+gzip $file.r1.fq
+gzip $file.r2.fq
+
+echo "gzip done " `date`
+mv $file.r1.fq.gz ~/work/kristin_nimblegen/input
+mv $file.r2.fq.gz ~/work/kristin_nimblegen/input
+
+rm /scratch/${file}_dir/*
+
+echo "End " `date`
