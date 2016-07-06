8e96127a59b57b37571ffd7cf7022221bab77db4
naumenko-sa <evolgenomicslab@gmail.com>
2016-07-06 15:14:17 -0400

    modified:   vcf2txt.sh

--- vcf2txt.sh
+++ vcf2txt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#only sites with variants because variants are usually the same, only few like A - ATTT in one sample  A - A/ATTTT in other
 
-gunzip -c $1 | grep -v "^#" |  awk '{print $1"-"$2"-"$4"-"$5}'
+gunzip -c $1 | grep -v "^#" |  awk '{print $1"-"$2}'
