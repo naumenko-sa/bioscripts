08ddddcbad61e0549431de8c255b7bc7326a6682
naumenko-sa <evolgenomicslab@gmail.com>
2016-07-08 12:00:15 -0400

    modified:   dexpression.athaliana.R

--- dexpression.athaliana.R
+++ dexpression.athaliana.R
@@ -1,6 +1,7 @@
 # 2016-06-15: DE of miRNA for A.thaliana project
 
 # https://bioconductor.org/packages/release/bioc/vignettes/edgeR/inst/doc/edgeRUsersGuide.pdf
+# http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4387895/
 library(edgeR)
 setwd("~/Desktop/project_micro_RNA_athaliana/dexpression")
 
@@ -8,24 +9,74 @@ setwd("~/Desktop/project_micro_RNA_athaliana/dexpression")
 
 all_counts=read.delim("counts_mirna.tsv",row.names="mirna")
 
+exploratory = function()
+{
+    #test:
+    x=all_counts[c("X12T3R1","X12T3R2","X12T3R3","X12T4R1","X12T4R2","X12T4R3")]
+    x=all_counts
+    group=factor(rep(1,35))
+    group=factor(c(1,1,1,2,2,2))
+    output="test1"
+    y=DGEList(counts=x,group=group)
+    
+    #filtration
+    #keep=rowSums(cpm(y)>2)==35
+    keep=rowSums(cpm(y)>1) >=4
+    y=y[keep,,keep.lib.sizes=FALSE]
+    
+    #mdsPlot
+    colors=substr(colnames(y$counts),1,3)
+    colors=gsub("X12","red",colors)
+    colors=gsub("X1T","green",colors)
+    colors=gsub("X6T","blue",colors)
+  
+    plotMDS(y,las=1,col=colors,main="MDS plot for raw data",xlim=c(-0.4,0.4),ylim=c(-0.4,0.4))
+    plotMDS(y,las=1,col=colors,main="MDS plot for raw data")
+    
+    nc=cpm(y,normalized.lib.sizes=F)
+    write.table(nc,"filtered.normalized_counts.txt",col.names=NA)
+    
+    plotMDS(nc,las=1,col=colors,main="MDS plot for CPM")    
+    
+    y=calcNormFactors(y)
+    write.table(y$samples,"filtered.norm.factors.txt",col.names=NA)    
+    plotMDS(y,las=1,col=colors,main="MDS plot for normalized data",ylim=c(0.4,-0.4))
+    plotMDS(y,las=1,col=colors,main="MDS plot for normalized data")
+    
+    
+}
+
 de_comparison = function(x,group,output)
 {
     #test:
     #x=all_counts[c("X12T3R1","X12T3R2","X12T3R3","X12T4R1","X12T4R2","X12T4R3")]
+    #x=all_counts
+    #group=factor(rep(1,35))
     #group=factor(c(1,1,1,2,2,2))
     #output="test1"
   
     y=DGEList(counts=x,group=group)
     
     #filtration
-    keep=rowSums(cpm(y)>2) >=2
+    #keep=rowSums(cpm(y)>2)==35
+    keep=rowSums(cpm(y)>1) >=4
     y=y[keep,,keep.lib.sizes=FALSE]
     
     #normalized counts
     nc=cpm(y,normalized.lib.sizes=F)
     write.table(nc,paste0(output,".normalized_counts.txt"),col.names=NA)
     
+    png(paste0(output,".mds.raw.png"))
+    plotMDS(y,las=1,main=paste0(output," - MDS plot for raw data"))
+    dev.off()
+    
     y=calcNormFactors(y)
+    
+    png(paste0(output,".mds.norm.png"))
+    plotMDS(y,las=1,main=paste0(output," - MDS plot for normalized data"))
+    dev.off()
+    
+    write.table(y$samples,paste0(output,".norm.factors.txt"),col.names=NA)
     
     design=model.matrix(~group)
     y=estimateDisp(y,design)
