e35076fd65f95d1c5d71fcf64046e2d07192d118
naumenko-sa <sergey_bulk@list.ru>
2016-07-26 14:42:23 -0400

    new file:   go.snpeff.sh

---
+++ go.snpeff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -l walltime=5:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g
+
+#without -csvStats to produce html reports
+#csvStats for multiqc
+
+for f in *.vcf;
+do
+    java -Xmx4G -jar /hpf/tools/centos6/snpEff/4.0/snpEff.jar GRCh37.75 $f -s ${f}.report.html -csvStats > `echo $f | sed s/vcf/ann.vcf/`;
+done;
