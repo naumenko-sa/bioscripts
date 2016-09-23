ab3288320b4cff56124a9f4b1b7711ed2d73a9b8
naumenko-sa <evolgenomicslab@gmail.com>
2016-09-23 11:48:21 -0400

    modified:   cheo.dp.filter.sh 	modified:   go.bcbio.sh 	modified:   go.blastn.sh 	new file:   vcfvep2txt.sh

--- cheo.dp.filter.sh
+++ cheo.dp.filter.sh
@@ -2,5 +2,5 @@
 
 module load gatk
 java -Xmx4g -jar $GATK -R /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa \
-     -T VariantFiltration -V $1 -o `echo $1 | sed s/vcf/dpfiltered.vcf/` \
-     --filterExpression " DP >= 10" --filterName "DP>=10"
+     -T VariantFiltration -V $1 -o `echo $1 | sed s/vcf/DPLT10.vcf/` \
+     --filterExpression " DP < 10" --filterName "DPLT10"
--- go.blastn.sh
+++ go.blastn.sh
@@ -13,7 +13,7 @@ base=$2
 num_hits=$3
 ev=$4
 
-blastn -task blastn -query $qry -db $base -out ${qry}_vs_${base}.blastn.${ev} -evalue $ev -outfmt "6 qseqid sseqid length pident qstart qend sstart send evalue bitscore" -num_alignments $num_hits -num_threads 20
+blastn -task blastn -query $qry -db $base -out ${qry}_vs_${base}.blastn.${ev} -evalue $ev -outfmt "6 qseqid sseqid length pident qstart qend sstart send evalue bitscore mismatch" -num_alignments $num_hits -num_threads 20
 
 date
 
