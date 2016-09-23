590536dd2fed9a05f5cbc22ceba06ccf5b23a9d2
naumenko-sa <sergey_bulk@list.ru>
2016-09-23 11:48:21 -0400

    modified:   cheo.dp.filter.sh 	modified:   go.bcbio.sh 	modified:   go.blastn.sh 	new file:   vcfvep2txt.sh

--- go.bcbio.sh
+++ go.bcbio.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-#PBS -l walltime=24:00:00,nodes=1:ppn=10
+#PBS -l walltime=10:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
-#PBS -l vmem=100g
+#PBS -l vmem=50g,mem=50g
 
-module load gatk/3.6.0
+#module load gatk/3.6.0
+#module load java/1.7.0
 
 echo "START: " `date`;
 
-bcbio_nextgen.py ../config/${project}.yaml -n 10
+module load java/1.8.0_91
+#module load gatk
+java -Xmx1g -version
+which java
+
+echo $project $threads
+
+bcbio_nextgen.py ../config/${project}.yaml -n $threads
 
 echo "END: " `date`;
 
