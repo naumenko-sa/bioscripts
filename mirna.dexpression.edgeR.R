6ed7305c3c18ae0c230d440f7861ae8875a125ba
naumenko-sa <evolgenomicslab@gmail.com>
2016-10-18 03:29:51 -0400

    modified:   mirna.dexpression.edgeR.R

--- mirna.dexpression.edgeR.R
+++ mirna.dexpression.edgeR.R
@@ -1,83 +1,137 @@
 #micro RNA dif expression analysis for potato/soybean project
 
-setwd("~/Desktop/project_microRNA_potato/2016-10-07_analysis/")
+setwd("~/Desktop/project_microRNA_potato/2016-10-18-report/")
 library(edgeR)
 
 #filter conservative micro RNAs
-glymax.conservative = read.table("glymax.conservative", row.names=NULL, stringsAsFactors=FALSE)
-glymax.all = read.delim("glymax.tsv", row.names=1, stringsAsFactors=FALSE)
-glymax.noncons = setdiff(glymax.all,glymax.conservative)
-write.table(glymax.noncons,"glymax.noncons.txt",quote=F,col.names=NA)
+create_nonconservative_sets = function ()
+{
+    glymax.conservative = read.table("glymax.conservative", row.names=NULL, stringsAsFactors=FALSE)
+    glymax.all = read.delim("glymax.tsv", row.names=1, stringsAsFactors=FALSE)
+    glymax.noncons = setdiff(glymax.all,glymax.conservative)
+    write.table(glymax.noncons,"glymax.noncons.txt",quote=F,col.names=NA)
 
-soltub.conservative = read.table("soltub.conservative", row.names=NULL, stringsAsFactors=FALSE)
-soltub.all = read.delim("soltub.tsv", row.names=1, stringsAsFactors=FALSE)
-soltub.noncons = setdiff(soltub.all,soltub.conservative)
-write.table(soltub.noncons,"soltub.noncons.txt",quote=F,col.names=NA)
+    soltub.conservative = read.table("soltub.conservative", row.names=NULL, stringsAsFactors=FALSE)
+    soltub.all = read.delim("soltub.tsv", row.names=1, stringsAsFactors=FALSE)
+    soltub.noncons = setdiff(soltub.all,soltub.conservative)
+    write.table(soltub.noncons,"soltub.noncons.txt",quote=F,col.names=NA)
+}
 
-gg=glymax.noncons[,"R4226"]
-gs=glymax.noncons[,"R4227"]
+exploratory_analysis = function()
+{
+  #test
+  all.counts = soltub.noncons
+  samples = c("R4226","R4227","R4228","R4230")
+  raw_counts = all.counts[samples]
+  
+  group=factor(c(1,1,1,1))
+  
+  y=DGEList(counts=raw_counts,group=group)
+  
+  keep = rowSums(cpm(y)>2) >=4
+  table(keep)
+  
+  y= y[keep,,keep.lib.sizes=F]
+  
+  plotMDS(y)
+}
 
-ss=soltub.noncons[,"R4227"]
-sg=soltub.noncons[,"R4226"]
+read_nonconservative_sets = function()
+{
+  glymax.noncons = read.table("glymax.noncons.txt",stringsAsFactors = F)
+  soltub.noncons = read.table("soltub.noncons.txt",stringsAsFactors=F)
+}
 
-tt=ss
+#all.counts = soltub.noncons | glymax.noncons
+#background = soltub | glymax
+pairwise_comparison = function(sample1,sample2,all.counts,background)
+{
+  #test
+  #sample1="R4227"
+  #sample2="R4228"
+  #all.counts=soltub.noncons
+  #background="soltub"
+  
+  raw_counts = all.counts[c(sample1,sample2)]
+  row.number = nrow(raw_counts)
 
-plot(soltub.noncons[,"R4226"])
+  group=factor(c(1,2))
+  
+  y=DGEList(counts=raw_counts,group=group)
 
-plot(glymax.noncons[,"R4227"])
-plot(soltub.noncons[,"R4227"])
+  y=calcNormFactors(y)
 
-g.control=glymax.noncons[c("R4226","R4227")]
-s.control=soltub.noncons[c("R4226","R4227")]
-x=s.control
+  design=model.matrix(~group)
 
-x = soltub.noncons[c("R4226","R4227","R4228")]
-x = glymax.noncons[c("R4226","R4227","R4228")]
+  bcv=0.2
+  #from A.thaliana experiment
+  dispersion=0.04
 
-#x=all_counts[c("X1T1R1","X1T1R2","X1T1R3","X1T2R1","X1T2R2","X1T2R3")]
-#x=all_counts[c("X1T1R1","X1T1R2","X1T1R3","X1T3R1","X1T3R2","X1T3R3")]
-x=all_counts[c("R4215", "R4216", "R4217", "R4218")]
-x=all_counts[c("R4215", "R4216", "R4217", "R4218","R4219","R4220")]
-x=all_counts[c("R4226", "R4227", "R4228", "R4229","R4230","R4231")]
+  #y=estimateDisp(y,design)
+
+  fit=glmFit(y,design,dispersion)
+  lrt=glmLRT(fit,coef=2)
+  topTags(lrt,n=row.number)
+
+  top_tags=topTags(lrt,n=row.number)
+
+  o=order(lrt$table$PValue)
+  normalized_counts=cpm(y)[o[1:row.number],]
+
+  raw_counts=raw_counts[o[1:row.number],]
+  
+  result=cbind(top_tags,normalized_counts,raw_counts)
+  filename=paste0(sample1,"_",sample2,".",background,".result.txt")
+  write.table(result,filename,quote=F,col.names=NA)
+}
+
+fourfold_comparison = function(samples,all.counts,background)
+{
+  #test
+  samples = c("R4226","R4228","R4227","R4230")
+  #all.counts=soltub.noncons
+  #background="soltub"
+  
+  raw_counts = all.counts[samples]
+  row.number = nrow(raw_counts)
+  
+  group=factor(c(1,1,2,2))
+  
+  y=DGEList(counts=raw_counts,group=group)
+  
+  y=calcNormFactors(y)
+  
+  design=model.matrix(~group)
+  
+  bcv=0.2
+  #from A.thaliana experiment
+  dispersion=0.04
+  
+  #y=estimateDisp(y,design)
+  
+  fit=glmFit(y,design,dispersion)
+  lrt=glmLRT(fit,coef=2)
+  topTags(lrt,n=row.number)
+  
+  top_tags=topTags(lrt,n=row.number)
+  
+  o=order(lrt$table$PValue)
+  normalized_counts=cpm(y)[o[1:row.number],]
+  
+  raw_counts=raw_counts[o[1:row.number],]
+  
+  result=cbind(top_tags,normalized_counts,raw_counts)
+  filename=paste0(paste(samples,collapse="_"),".",background,".result.txt")
+  write.table(result,filename,quote=F,col.names=NA)
+}
+
+read_nonconservative_sets()
+pairwise_comparison("R4226","R4227",soltub.noncons,"soltub")
+pairwise_comparison("R4226","R4227",glymax.noncons,"glymax")
 
 
-group=factor(c(1,1,1))
-group=factor(c(1,1,2,2))
-group=factor(c(1,1,1,1))
-group=factor(c(1,1,1,1,1,1))
-#group=factor(c(1,1,1,2,2,2))
+pairwise_comparison("R4228","R4227",soltub.noncons,"soltub")
+pairwise_comparison("R4226","R4228",soltub.noncons,"soltub")
 
-y=DGEList(counts=x,group=group)
-
-plotMDS(y)
-plotMDS(y,cex=1.5,cex.lab=1.5,cex.axis=1.5)
-
-y=calcNormFactors(y)
-
-design=model.matrix(~group)
-
-bcv=0.2
-#from A.thaliana experiment
-dispersion=0.04
-
-#y=estimateDisp(y,design)
-
-fit=glmFit(y,design,dispersion)
-lrt=glmLRT(fit,coef=2)
-topTags(lrt,n=262)
-
-top_tags=topTags(lrt,n=262)$table
-
-write.table(top_tags,"R4228.soltub.txt",quote=F,col.names=NA)
-write.table(top_tags,"R4228.glymax.txt",quote=F,col.names=NA)
-
-
-
-
-o=order(lrt$table$PValue)
-normalized_counts=cpm(y)[o[1:262],]
-raw_counts=x[o[1:262],]
-
-result=cbind(top_tags,normalized_counts,raw_counts)
-filename=paste0(paste(colnames(x),collapse='_'),".txt")
-write.table(result,filename)
+pairwise_comparison("R4230","R4227",soltub.noncons,"soltub")
+pairwise_comparison("R4226","R4230",soltub.noncons,"soltub")
