249aa3f8d217926709efb0a40b6c8d8606b03228
naumenko-sa <sergey_bulk@list.ru>
2016-10-26 18:14:47 -0400

    modified:   fastq.nreads.sh 	new file:   go.bcbio.parallel.sh 	modified:   test.pbs 	new file:   test.pbs.o8707936 	new file:   test.pbs.o8707937 	new file:   test.pbs.o8707938 	new file:   test.pbs.o8707941 	new file:   test.pbs.o8723009 	new file:   test.pbs.o8723052 	new file:   vcf.validate1.sh

--- fastq.nreads.sh
+++ fastq.nreads.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cat $1 | wc -l | awk '{print $0/4}' > $1.nreads
+#PBS -l walltime=2:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
+
+
+zcat $file | wc -l | awk '{print $0/4}' > $file.nreads
