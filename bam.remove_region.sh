90d2b8a6abfd32061101daedc77cbb308032f2e5
naumenko-sa <evolgenomicslab@gmail.com>
2016-11-04 15:25:21 -0400

    new file:   bam.remove_region.sh

---
+++ bam.remove_region.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#removes reads associated with a specific region from a bam file
+
+# $bam - input.bam
+# $bed - filter.bed
+# output - input.filtered.bam
+
+#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
+
+module load bedtools
+
+bedtools intersect -abam $bam -b $bed -v > `echo $bam | s s/filtered.bam/`
