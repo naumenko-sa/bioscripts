697f578bc03c7e8501c237d89886e423b59c0cba
naumenko-sa <evolgenomicslab@gmail.com>
2016-12-11 20:37:15 -0500

    modified:   vcf.freebayes.getAO.sh 	new file:   vcf.gatk.get_depth.sh 	modified:   vcf.platypus.getNV.sh 	deleted:    alignment.count_nongappy_columns.sh 	deleted:    bcbio.prepare_config.sh 	deleted:    cheo.validate.sh 	deleted:    fastq.remove_duplicates.sh 	deleted:    go.bcbio1.sh 	deleted:    go.gatk.all.pbs 	deleted:    plot_diagram.R 	deleted:    pseudoatosomal.txt 	deleted:    vcf2spectrum.sh

--- vcf.gatk.get_depth.sh
+++ vcf.gatk.get_depth.sh
@@ -3,4 +3,4 @@
 bname=`echo $1 | sed s/.vcf.gz//`
 
 gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
-     -F CHROM -F POS -F REF -F ALT -F DP -GF DP -GF AO -o $bname.table --allowMissingData 
+     -F CHROM -F POS -F REF -F ALT -F DP -GF DP -GF AD -o $bname.table --allowMissingData 
--- vcf.platypus.getNV.sh
+++ vcf.platypus.getNV.sh
@@ -3,4 +3,4 @@
 bname=`echo $1 | sed s/.vcf.gz//`
 
 gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
-     -F CHROM -F POS -F REF -F ALT -GF NR -GF NV -o $bname.table --allowMissingData 
+     -F CHROM -F POS -F REF -F ALT -F TC -GF NR -GF NV -o $bname.table --allowMissingData 
