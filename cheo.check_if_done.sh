b529fb37b1a48c62de6c93b82799b6242e756cad
naumenko-sa <evolgenomicslab@gmail.com>
2017-01-03 12:54:53 -0500

    modified:   cheo.check_if_done.sh 	modified:   gemini.vep.sh

--- cheo.check_if_done.sh
+++ cheo.check_if_done.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #list successfully finished bcbio runs only with family name
+#for f in go.bcbio_array*;do cheo.check_if_done.sh $f >> ready.txt;done;
+#while read log family;do mv $family ../0done/;done < ready.txt
+#while read family;do qsub ~/bioscripts/cheo.postprocess.sh -v family=$family;done < families.txt 
+#for f in `cat families.txt`;do mkdir 2report/$f;cp $f/*.txt 2report/$f;cp $f/*.table 2report/$f;done;
 
 if grep -q "Timing: finished" $1;
 then
