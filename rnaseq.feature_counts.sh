db1409f780f30b8e63c8f0e62b6a4954bbaa08e7
naumenko-sa <evolgenomicslab@gmail.com>
2017-01-16 18:09:50 -0500

    new file:   rnaseq.feature_counts.sh 	modified:   rnaseq.qorts.get_novel_exons.sh

---
+++ rnaseq.feature_counts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#PBS -l walltime=2:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
+
+if [ -z bam ];
+then
+    bam=$1
+fi
+    
+
+featureCounts  \
+	-T 8 \ #threads
+	-F GTF 
+	-t exon 
+	-g gene_id 
+	-s 0 
+	-C \
+	--largestOverlap \
+	-a /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/rnaseq/ref-transcripts.gtf \
+	-o $1.counts.txt \
+	$1
--- rnaseq.qorts.get_novel_exons.sh
+++ rnaseq.qorts.get_novel_exons.sh
@@ -19,5 +19,9 @@ bedtools intersect -wa -wb -a $1.novel_junctions.bed -b ~/Desktop/reference_tabl
 #get novel exons $2 = 100 or 200
 cat $1.results.bed | awk 'BEGIN{prev=0}{print $0"\t"$2-prev;prev=$3}' | awk -v threshold=$2 '{if ($6>0 && $6<threshold) print $0;}' > $1.novel_exons
 
+#report:
+echo "Junctions:" `cat $1.results.bed | wc -l`
+echo "Exons:" `cat $1.novel_exons | wc -l`
+
 gunzip -c QC.spliceJunctionCounts.knownSplices.txt.gz | sed 1d | cut -f 2,4,5,6 > $1.known_junctions.bed
 bedtools intersect -wa -wb -a $1.known_junctions.bed -b ~/Desktop/reference_tables/muscular_genes.bed | awk '{print $1"\t"$2"\t"$3"\t"$8"\t"$4}' | awk '{if ($5<2) print $0}' > $1.known.low_coverage
