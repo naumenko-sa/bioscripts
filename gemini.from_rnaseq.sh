23f285162e1a44373bdaa2c341de10ba9568be07
naumenko-sa <evolgenomicslab@gmail.com>
2017-01-20 16:24:12 -0500

    modified:   gemini.from_rnaseq.sh

--- gemini.from_rnaseq.sh
+++ gemini.from_rnaseq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=2:00:00,nodes=1:ppn=16
+#PBS -l walltime=2:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=50g,mem=50g
@@ -11,13 +11,18 @@ then
     sample=$1
 fi
 
-#gunzip -c combinedrnaedit-annotated.vcf.gz | grep "^#"  > $sample.vcf
-#gunzip -c combinedrnaedit-annotated.vcf.gz | grep -v "^#" | grep PASS | grep -v possible_rnaedit  >> $sample.vcf
+if [ -z $vcf ]
+then
+    vcf=$2
+fi
 
-#bgzip $sample.vcf
-#tabix $sample.vcf.gz
+gunzip -c $vcf | grep "^#"  > $sample.vcf
+gunzip -c $vcf | grep -v "^#" | grep PASS | grep -v possible_rnaedit  >> $sample.vcf
 
-#gemini.decompose.sh $sample.vcf.gz
-#gemini.vep.sh $sample.decomposed.vcf.gz 
+bgzip $sample.vcf
+tabix $sample.vcf.gz
+
+gemini.decompose.sh $sample.vcf.gz
+gemini.vep.sh $sample.decomposed.vcf.gz 
 gemini.vep2gemini.sh $sample.decomposed.vepeffects.vcf.gz
 gemini.gemini2txt.sh $sample.decomposed.vepeffects.db
