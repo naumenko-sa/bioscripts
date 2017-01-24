9ff4a0684548a727fbb46cbcb5f4c4b2eac85518
naumenko-sa <evolgenomicslab@gmail.com>
2017-01-23 23:00:09 -0500

    new file:   cram2fq.sh

---
+++ cram2fq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=20g,mem=20g
+
+module load java
+
+cramtools fastq -Xmx10g -F $sample --skip-md5-check \
+		-z \
+		-I $cram \
+		-R /hpf/largeprojects/ccm/forgedata/c4r_unsolved_clinical_exome/1092R/hg19.fa.gz  --max-records 10000
