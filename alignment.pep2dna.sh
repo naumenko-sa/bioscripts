722f00090086c71ae902cc33ca70d1604c0959ff
naumenko-sa <evolgenomicslab@gmail.com>
2017-02-04 22:50:34 -0500

    modified:   README.md 	modified:   alignment.pep2dna.sh

--- alignment.pep2dna.sh
+++ alignment.pep2dna.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-cat $1 | grep '>' | sed s/">"// > $1.names;
-faSomeRecords cds.fasta $1.names `echo $1 | sed s/aln/cds/`;
-/home/tools/RevTrans-1.4/revtrans.py -match pos `echo $1 | sed s/aln/cds/` $1 $1.fa;
+# reverse translates amino acid alignment in to DNA
+# uses faSomeRecords from Kent tools and revtrans
+# cds.fasta is a database of all cds sequences
+
+# $1 - name.aln.fasta
+# result = name.cds.aln
+
+cat $1 | grep '>' | sed s/">"// > $1.names
+
+cds_file=`echo $1 | sed s/aln/cds/`
+
+#makes unaligned file with CDS sequences
+faSomeRecords cds.fasta $1.names $cds_file
+
+/home/tools/RevTrans-1.4/revtrans.py -match pos $cds_file $1 $1.fa;
+
 align2fasta.pl $1.fa >`echo $1 | sed s/aln/cds.aln/`;
-rm $1.fa $1.names `echo $1 | sed s/aln/cds/`;
+
+rm $1.fa $1.names $cds_file
