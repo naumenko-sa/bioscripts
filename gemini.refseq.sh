003a6c4d3ca1d13560731540cf89a58554944c90
naumenko-sa <evolgenomicslab@gmail.com>
2017-03-09 12:42:10 -0500

    new file:   gemini.refseq.sh 	deleted:    gemini.variant_effects.sh 	deleted:    gemini2txt.pbs

---
+++ gemini.refseq.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# creates gemini database with vep refseq annotations, 
+# dumps text file and variant_impacts for rare harmful variants
+
+#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
+
+
+vt rminfo ${family}-ensemble.vcf.gz -t CSQ -o ${family}.no_vep.vcf.gz
+
+gemini.decompose.sh ${family}.no_vep.vcf.gz
+
+gemini.vep.refseq.sh ${family}.no_vep.decomposed.vcf.gz
+
+gemini.vep2gemini.sh ${family}.no_vep.decomposed.vepeffects_refseq.vcf.gz
+
+gemini.gemini2txt.sh ${family}.no_vep.decomposed.vepeffects_refseq.db
+
+gemini.variant_impacts.sh ${family}.no_vep.decomposed.vepeffects_refseq.db
