77a34bf52ced31c08344fca53fd89618463c8668
naumenko-sa <evolgenomicslab@gmail.com>
2017-03-10 10:29:50 -0500

    modified:   gemini.bug.parse_vep.pl 	modified:   gemini.bug.workaround.sh

--- gemini.bug.parse_vep.pl
+++ gemini.bug.parse_vep.pl
@@ -1,26 +1,34 @@
 #!/usr/bin/perl
 
+# parses vcf with VEP annotation - CSQ field
+
 open(IN,$ARGV[0]);
 
 while (<IN>)
 {
     chomp;
+    $impacts_string='';
     if ($_ !~ /CHROM/)
     {
 	@ar = split("\t",$_);
 	$id = $ar[0]."-".$ar[1]."-".$ar[2]."-".$ar[3];
 	@ar2 = split(",",@ar[4]);
-	print $id."\n";
+	#print $id."\n";
 	foreach $impact (@ar2)
 	{
 	    #print $impact."\n";
 	    @ar3= split(/\|/,$impact);
-	    if ($ar3[17] == '')
+	    #if there is hvgsc
+	    if ($ar3[16] ne '')
 	    {
-		push(@ar3,'NA');
+		if ($ar3[17] eq '')
+		{
+		    $ar3[17] = 'NA';
+		}
+		$impacts_string .= "exon".$ar3[9].":".$ar3[16].":".$ar3[17].",";
 	    }
-	    print "exon",$ar3[9].":".$ar3[16].":".$ar3[17]."\n";
 	}
+	print $id."\t".substr($impacts_string,0,-1)."\n";
     }
 }
 
--- gemini.bug.workaround.sh
+++ gemini.bug.workaround.sh
@@ -9,7 +9,11 @@ then
     family=$1
 fi
 
+#extract positions of the variants in the final report
 cat ${family}-ensemble.db.txt  | awk -F "\t" '{print $23"\t"$24"\t"$24}' | sed 1d | sed s/chr// > $family.bug.bed
+#extract variants with their VEP annotations
 bedtools intersect -header -a ${family}-ensemble.vcf.gz -b $family.bug.bed > $family.bug.vcf
+#convert vcf to table
 gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V ${family}.bug.vcf -F CHROM -F POS -F REF -F ALT -F CSQ -o $family.bug.table
+#parse vcf and VEP CSQ field
 gemini.bug.parse_vep.pl $family.bug.table > $family.impacts.hgvs
