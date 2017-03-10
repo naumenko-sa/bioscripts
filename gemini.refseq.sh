4fd76b4937ff709fafcea07d1e1b9c8e64730de1
naumenko-sa <evolgenomicslab@gmail.com>
2017-03-09 22:38:05 -0500

    modified:   gemini.refseq.sh 	new file:   gemini.refseq.variant_impacts.sh

--- gemini.refseq.sh
+++ gemini.refseq.sh
@@ -3,17 +3,17 @@
 # creates gemini database with vep refseq annotations, 
 # dumps text file and variant_impacts for rare harmful variants
 
-#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -l walltime=10:00:00,nodes=1:ppn=10
 #PBS -joe .
 #PBS -d .
-#PBS -l vmem=10g,mem=10g
+#PBS -l vmem=50g,mem=50g
 
 
-vt rminfo ${family}-ensemble.vcf.gz -t CSQ -o ${family}.no_vep.vcf.gz
+#vt rminfo ${family}-ensemble.vcf.gz -t CSQ -o ${family}.no_vep.vcf.gz
 
-gemini.decompose.sh ${family}.no_vep.vcf.gz
+#gemini.decompose.sh ${family}.no_vep.vcf.gz
 
-gemini.vep.refseq.sh ${family}.no_vep.decomposed.vcf.gz
+#gemini.vep.refseq.sh ${family}.no_vep.decomposed.vcf.gz
 
 gemini.vep2gemini.sh ${family}.no_vep.decomposed.vepeffects_refseq.vcf.gz
 
