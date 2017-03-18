f8bf6071cbd52fb1ea7f9f568b14d3662c7e9f88
naumenko-sa <evolgenomicslab@gmail.com>
2017-03-18 12:12:39 -0400

    modified:   bcbio.cleanup.sh

--- bcbio.cleanup.sh
+++ bcbio.cleanup.sh
@@ -11,7 +11,7 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
-#PBS -l vmem=10g,mem=10g
+#PBS -l vmem=20g,mem=20g
 
 function cleanup
 {
@@ -77,5 +77,5 @@ then
 fi
 
 echo $family
-cleanup
+#cleanup
 prepare_for_report
