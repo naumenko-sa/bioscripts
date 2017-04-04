6d89bae8c5ad42c16cd809ea65f1e8935a8084e7
naumenko-sa <evolgenomicslab@gmail.com>
2017-04-04 16:55:48 -0400

    new file:   bam.coverage.sh 	modified:   bcbio.cleanup.sh 	deleted:    bam.coverage.pbs

---
+++ bam.coverage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#calculates coverage of a bed file with mapped reads from bam file
+
+# $bam - input.bam
+# $bed - filter.bed
+# output - input.bam.coverage
+
+#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
+
+#module load bedtools
+#uses bedtools from bcbio
+
+if [ -z $bam ]
+then
+    bam=$1
+fi
+
+if [ -z $bed ]
+then
+    bed=$2
+fi
+
+bedtools coverage -hist -abam $bam -b $bed > $bam.coverage
