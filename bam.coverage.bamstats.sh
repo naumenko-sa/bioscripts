707e7ca2dc24ab36fa0e3bb69e75b4443042a54b
naumenko-sa <evolgenomicslab@gmail.com>
2017-04-05 09:52:29 -0400

    modified:   bam.coverage.bamstats.sh

--- bam.coverage.bamstats.sh
+++ bam.coverage.bamstats.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#https://github.com/lindenb/jvarkit/wiki/BamStats04
-#uses bamstats05 - grouping coverage by gene
-#arguments: bam and bed
+# https://github.com/lindenb/jvarkit/wiki/BamStats05
+# uses bamstats05 - groups coverage by gene
+# really fast: 2min for a big bam file and 100 genes
+# arguments: bam and bed
 
 #PBS -l walltime=2:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=21g
 
+#wants java1.8
 module load java
 java -Xmx10G -jar ~/work/tools/jvarkit/dist/bamstats05.jar \
     -B=$bed $bam > $bam.coverage;
