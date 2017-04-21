0d8d09829ef430e55e70b271947bb2af30df0785
naumenko-sa <evolgenomicslab@gmail.com>
2017-04-20 20:12:07 -0400

    deleted:    bcbio.array.pbs 	deleted:    bcbio.cleanup.sh 	deleted:    bcbio.pbs 	deleted:    cheo.dp.filter.sh~ 	deleted:    cheo.orphanet.parse.sh 	deleted:    gemini.gemini2report.R 	deleted:    gemini.gemini2txt.sh 	deleted:    gemini.refseq.sh 	deleted:    gemini.variant_impacts.sh 	deleted:    vcf.freebayes.getAO.sh 	deleted:    vcf.gatk.get_depth.sh 	deleted:    vcf.platypus.getNV.sh

--- vcf.freebayes.getAO.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bname=`echo $1 | sed s/.vcf.gz//`
-
-gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
-     -F CHROM -F POS -F REF -F ALT -F DP -GF DP -GF AO -o $bname.table --allowMissingData 
--- vcf.platypus.getNV.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bname=`echo $1 | sed s/.vcf.gz//`
-
-gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
-     -F CHROM -F POS -F REF -F ALT -F TC -GF NR -GF NV -o $bname.table --allowMissingData 
