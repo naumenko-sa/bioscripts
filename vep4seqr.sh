2d4ea6cf7199b5936e85a984d9b5baaa51b6d9f1
naumenko-sa <evolgenomicslab@gmail.com>
2017-05-07 10:43:24 -0400

    modified:   vep4seqr.sh 	deleted:    sam2sortedbam.sh

--- vep4seqr.sh
+++ vep4seqr.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
-# vep annotation for improt to seqr
+# vep annotation for import to seqr
+# https://github.com/macarthur-lab/seqr/blob/master/deploy/mac_osx/README.md#load-your-own-data
 
 #PBS -l walltime=2:00:00,nodes=1:ppn=1
 #PBS -joe .
@@ -15,7 +16,7 @@ fi
 bname=`echo $vcf | sed s/.vcf.gz//`
 
 unset PERL5LIB && export PATH=/hpf/largeprojects/ccmbio/naumenko/tools/bcbio/anaconda/bin:$PATH && /home/naumenko/work/tools/bcbio/anaconda/bin/variant_effect_predictor.pl \
-    --everything --vcf --allele_number --no_stats --cache --offline --force_overwrite --cache_version 81 --assembly GRCh37 --tabix \
+    --everything --vcf --allele_number --no_stats --cache --offline --force_overwrite --cache_version 87 --assembly GRCh37 --tabix \
     --plugin LoF,human_ancestor_fa:/hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/variation/human_ancestor.fa.gz,filter_position:0.05,min_intron_size:15 \
     --plugin dbNSFP,/hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/variation/dbNSFP.txt.gz,Polyphen2_HVAR_pred,CADD_phred,SIFT_pred,FATHMM_pred,MutationTaster_pred,MetaSVM_pred \
     -i $vcf -o $bname.vep.vcf.gz \
