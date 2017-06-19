9f304586253dbba35145f9cfe80ee42bd3de5824
naumenko-sa <evolgenomicslab@gmail.com>
2017-06-19 17:56:43 -0400

    deleted:    alignment.check_3x.sh~ 	new file:   bcbio.install.sh 	new file:   bcbio.upgrade.sh 	deleted:    cheo.c4r_database.sh 	deleted:    cheo.c4r_database_merge.pl 	deleted:    cheo.check_if_done.sh 	modified:   cheo.database.py 	deleted:    cheo.genetic_counselor.R 	deleted:    gemini.vep.sh 	deleted:    gemini.vep2gemini.sh 	deleted:    gemini.vep2gemini_all.sh 	deleted:    hla.athlates.sh~ 	deleted:    n50.sh~

---
+++ bcbio.upgrade.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bcbio_nextgen.py upgrade -u skip --datatarget rnaseq --datatarget gemini --datatarget cadd --genomes hg38
--- cheo.database.py
+++ cheo.database.py
@@ -16,12 +16,15 @@ for sample in samples:
     with open(family+".csv",'rb') as f_csv:
 	reader = csv.DictReader(f_csv)
 	with open(family+'.'+sample+'.c4r','w') as f_sample:
-	    fieldnames=['Position','Ref','Alt','Variation','Zygosity.'+sample,'Protein_change_ensembl','Gene','Conserved_in_29_mammals','Sift_score','Polyphen_score','Cadd_score']
+	    zygosity_field = 'Zygosity.'+sample
+	    #because R when building report substitutes - with . in column names
+	    zygosity_field = zygosity_field.replace("-",".")
+	    fieldnames=['Position','Ref','Alt','Variation',zygosity_field,'Protein_change_ensembl','Gene','Conserved_in_29_mammals','Sift_score','Polyphen_score','Cadd_score']
 	    for row in reader:
 		l = []
 		for key in fieldnames:
 		    l.append(row[key])
-		zygosity=row['Zygosity.'+sample]
+		zygosity=row[zygosity_field]
 		if (zygosity != '-' and not re.search('Insufficient',zygosity)):
 		    f_sample.write(','.join(l)+','+sample)
 		    f_sample.write('\n')
