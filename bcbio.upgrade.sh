023dd656de18ad5803c3bc2284f8c7bfcdc8bdcb
naumenko-sa <evolgenomicslab@gmail.com>
2017-07-06 12:12:17 -0400

    modified:   bcbio.upgrade.sh

--- bcbio.upgrade.sh
+++ bcbio.upgrade.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bcbio_nextgen.py upgrade -u skip --datatarget rnaseq --datatarget gemini --datatarget cadd --genomes hg38
+bcbio_nextgen.py upgrade -u skip --data --datatarget cadd
+#--datatarget rnaseq --datatarget gemini --datatarget cadd --genomes hg38
