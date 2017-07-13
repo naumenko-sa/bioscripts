da7a723d73ce3df5ca97052b236cabad5c14e456
naumenko-sa <evolgenomicslab@gmail.com>
2017-07-13 17:09:51 -0400

    modified:   bcbio.upgrade.sh 	deleted:    cheo.database.header 	deleted:    cheo.database.py 	deleted:    cheo.database_merge.py

--- bcbio.upgrade.sh
+++ bcbio.upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bcbio_nextgen.py upgrade --data --datatarget dbnsfp --genomes hg38
+bcbio_nextgen.py upgrade --data --datatarget vep --genomes hg38
 #--datatarget rnaseq --datatarget gemini --datatarget cadd --genomes hg38
