f366fa7da9e02793066dbfbb62ea19d8f5f0abc5
naumenko-sa <evolgenomicslab@gmail.com>
2017-08-28 18:31:55 -0400

    modified:   bam.reads_number.sh 	modified:   bcbio.upgrade.sh 	deleted:    bcbio.prepare_family.sh 	deleted:    gemini.from_rnaseq.sh

--- bcbio.upgrade.sh
+++ bcbio.upgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-bcbio_nextgen.py upgrade --data --datatarget vep --genomes hg38
+bcbio_nextgen.py upgrade --data --genomes hg19
+#--datatarget vep --genomes hg38
 #--datatarget rnaseq --datatarget gemini --datatarget cadd --genomes hg38
