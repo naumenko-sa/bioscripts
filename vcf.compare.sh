aa042ac95e78c135fd005dde10c84e357817f881
naumenko-sa <evolgenomicslab@gmail.com>
2018-06-26 15:29:02 -0400

    modified:   vcf.compare.sh

--- vcf.compare.sh
+++ vcf.compare.sh
@@ -2,17 +2,40 @@
 
 # compares 2 vcf files, needs bcftools in PATH
 
-for arg do
-    echo $arg
-    bcftools stats $arg | egrep "number of SNPs:" | awk '{print $NF"\tSNPs"}'
+function get_snps
+{
+    echo $1
+    bcftools stats $1 | egrep "number of SNPs:" | awk '{print $NF"\tSNPs"}'
+}
+
+function get_indels
+{
+    echo $1
     bcftools stats $arg | egrep "number of indels:" | awk '{print $NF"\tindels"}'
+}
+
+for arg do
+    get_snps $arg
+    get_indels $arg
 done
 
 mkdir _dir
 bcftools isec -p _dir $1 $2
 
-echo "Variants:"
-echo "Unique to" $1 `cat _dir/0000.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0000.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
-echo "Unique to" $2 `cat _dir/0001.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0001.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
-echo "Shared1" `cat _dir/0002.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0002.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
-echo "Shared2" `cat _dir/0003.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0003.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
+cd _dir
+
+for f in *.vcf;do bgzip $f;tabix $f.vcf.gz;done;
+
+cd ..
+
+echo "Unique to" $1 
+get_snps _dir/0000.vcf.gz
+get_indels _dir/0000.vcf.gz
+
+echo "Unique to" $2
+get_snps _dir/0001.vcf.gz
+get_indels _dir/0001.vcf.gz
+
+echo "Shared"
+get_snps _dir/0002.vcf.gz
+get_indels _dir/0002.vcf.gz
