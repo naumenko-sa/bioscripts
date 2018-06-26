e80fdf6bdb32392fac990c86d89e6078f3a97305
naumenko-sa <evolgenomicslab@gmail.com>
2018-06-26 15:07:13 -0400

    renamed:    vcf.compare2.sh -> vcf.compare.sh

--- vcf.compare.sh
+++ vcf.compare.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#compares 2 vcf files
+# compares 2 vcf files, needs bcftools in PATH
 
+for arg do
+    echo $arg
+    bcftools stats $arg | egrep "number of SNPs:" | awk '{print $NF"\tSNPs"}'
+    bcftools stats $arg | egrep "number of indels:" | awk '{print $NF"\tindels"}'
+done
 
-module load bcftools/1.3
 mkdir _dir
 bcftools isec -p _dir $1 $2
 
@@ -12,5 +16,3 @@ echo "Unique to" $1 `cat _dir/0000.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/
 echo "Unique to" $2 `cat _dir/0001.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0001.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
 echo "Shared1" `cat _dir/0002.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0002.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
 echo "Shared2" `cat _dir/0003.vcf | grep -c -v "^#"` "AVR QUAL=" `cat _dir/0003.vcf | grep -v "^#" | awk '{sum+=$6}END{print sum/NR}'`
-
-
