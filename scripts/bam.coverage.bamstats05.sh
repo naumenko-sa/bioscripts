05ee88f949aa0e161ec7795ac4162b124e33cd25
naumenko-sa <evolgenomicslab@gmail.com>
2019-03-18 11:49:49 -0400

    bam.coverage.bamstats05.sh

--- scripts/bam.coverage.bamstats05.sh
+++ scripts/bam.coverage.bamstats05.sh
@@ -4,6 +4,7 @@
 # uses bamstats05 - groups coverage by gene
 # really fast: 2min for a big bam file and 100 genes
 # arguments: bam and bed
+# by default uses protein coding genes
 
 # pull exone coordinates with genes.R and do bedtools sort | bedtools merge -c 4 -o first 
 
@@ -17,6 +18,11 @@
 BAMSTATS_PATH=/hpf/largeprojects/ccmbio/naumenko/tools/jvarkit/dist
 
 # if you need all reads, add -f "" - empty filter, by default it filters out some duplicated reads
+
+if [ -z $bed ]
+then
+    bed=~/cre/data/protein_coding_genes.exons.fixed.sorted.bed
+fi
 
 java -Xmx10G -jar ${BAMSTATS_PATH}/bamstats05.jar \
     -B $bed \
