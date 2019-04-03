ffdda3d64fbd3f92ef92508c33b7abf81cb86251
naumenko-sa <evolgenomicslab@gmail.com>
2019-04-03 15:30:32 -0400

    modified:   scripts/bam.coverage.sh

--- scripts/bam.coverage.sh
+++ scripts/bam.coverage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# $bam - input.bam
-# $bed - panel.bed
-# output - input.bam.coverage
-# http://bedtools.readthedocs.io/en/latest/content/tools/coverage.html
-
 #PBS -l walltime=10:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=2g,mem=2g
+
+# $bam - input.bam
+# $bed - panel.bed
+# output - input.bam.coverage
+# http://bedtools.readthedocs.io/en/latest/content/tools/coverage.html
+# takes ~30 min for 100 gene panel and WES
 
 if [ -z $bam ]
 then
@@ -43,7 +44,8 @@ fi
 
 echo "Start: " `date`
 
-#bedtools sort -faidx /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa.bedtoolsindex -i $bed > $bed.faidxsorted.bed
+# automatically sorting beds is not a good idea with muliple bams in the dir
+# bedtools sort -faidx /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa.bedtoolsindex -i $bed > $bed.faidxsorted.bed
 
 bedtools coverage -d -sorted -a $bed -b $bam -g /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa.bedtoolsindex $params > $bam.dcoverage
 
