da2113a52d1e4e92dcf4235ef403eaeded962981
naumenko-sa <evolgenomicslab@gmail.com>
2019-05-17 19:06:44 -0400

    sync

--- scripts/modeltest.sh
+++ scripts/modeltest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#PBS -l walltime=10:00:00,nodes=1:ppn=5
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
 
 # modeltest.sh file.fasta num_threads
-
-java -jar /hpf/largeprojects/ccmbio/naumenko/tools/jmodeltest-2.1.10/jModelTest.jar -d $1 -s 3 -g 4 -f -AIC -BIC -i -a -tr $2 -o $1.modeltest
+java -jar /hpf/largeprojects/ccmbio/naumenko/tools/jmodeltest-2.1.10/jModelTest.jar -d $align -s 3 -g 4 -f -AIC -BIC -i -a -tr 5 -o $align.modeltest
 #-s 11 -i
