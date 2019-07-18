dc156ec19975cee69816c41d83099604c1ba7dcc
naumenko.sa <evolgenomicslab@gmail.com>
2019-07-18 18:51:09 -0400

    sync

--- scripts/vcf.split_multisample_vcf.sh
+++ scripts/vcf.split_multisample_vcf.sh
@@ -2,7 +2,10 @@
 # split multisample vcf
 # https://www.biostars.org/p/138694/
 
-for sample in `bcftools query -l $file`; 
+bname=`basename $1 .vcf.gz`
+
+
+for sample in `bcftools query -l $1`
 do
-    bcftools view -c1 -Oz -s $sample -o ${file/.vcf*/.$sample.vcf.gz} $file
+    bcftools view -c1 -Oz -s $sample -o $bname.$sample.vcf.gz $1
 done
