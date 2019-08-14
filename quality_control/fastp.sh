522002e27351eb1ae21a434baea22c4fecbf0cb9
naumenko.sa <evolgenomicslab@gmail.com>
2019-08-14 11:21:41 -0400

    fastp

---
+++ quality_control/fastp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# fastp trimming was superior when working with rRNAdepletion libraries
+# $1 - left
+# $2 - right 
+# $3 - sample
+
+#SBATCH --partition=short        # Partition (queue)
+#SBATCH --time=05:00:00              # Runtime in D-HH:MM format
+#SBATCH --job-name=schumacher            # Job name
+#SBATCH -c 10
+#SBATCH --mem=10G            # Memory needed per CPU
+#SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
+#SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
+
+fastp -i $1 -I $2 -o $3.trimmed_1.fq.gz -O $3.trimmed_2.fq.gz -h $3.fastp_html -g -x -5 -3 -w 10
+
+#Additional flag definitions:
+#-g trims polyG, common artifact in Illumina 2-dye sequencing chemistry (MiniSeq, NextSeq, Novaseq)
+#-x trims polyX, any other homopolymeric stretch
+#-5 sliding window 5’ quality trimming
+#-3 sliding window 3’ quality trimming (I think these last two are similar to what trimmomatic does)
