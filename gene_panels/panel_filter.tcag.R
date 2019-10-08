b5942434d569fbc20d0a013e8b8568c1889b1d1f
naumenko-sa <evolgenomicslab@gmail.com>
2019-10-07 20:12:02 -0400

    added reports in xlsx

--- gene_panels/panel_filter.tcag.R
+++ gene_panels/panel_filter.tcag.R
@@ -2,10 +2,11 @@
 # Filter TCAG small variant report
 ###############################################################################
 library(tidyverse)
+library(writexl)
 
 args = commandArgs(trailingOnly = T)
 input_table <- read_tsv(args[1])
 panel <- read_csv(args[2])
 filtered_table <- input_table %>% filter(FILTER=="PASS", gene_symbol %in% panel$external_gene_name,
                                          !typeseq_priority %in% c("intronic"))
-write_excel_csv(filtered_table, args[3])
+write_xlsx(filtered_table, args[3])
