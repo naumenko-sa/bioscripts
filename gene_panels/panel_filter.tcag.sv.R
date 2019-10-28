603776c092225ba4645c1712818ecc20b69c65eb
naumenko-sa <evolgenomicslab@gmail.com>
2019-10-28 13:45:54 -0400

    modified:   gene_panels/panel_filter.tcag.sv.R

--- gene_panels/panel_filter.tcag.sv.R
+++ gene_panels/panel_filter.tcag.sv.R
@@ -1,10 +1,24 @@
 ###############################################################################
-# Filter TCAG small variant report
+# Filter TCAG CNV report
 ###############################################################################
 library(tidyverse)
 
 args = commandArgs(trailingOnly = T)
-input_table <- read_tsv(args[1])
+input_table <- read_tsv(args[1], col_types = cols(.default = "c"))
 panel <- read_csv(args[2])
-filtered_table <- input_table %>% filter(FILTER=="PASS", gene_symbol %in% panel$external_gene_name)
+
+# gene_symbol = "TPTE|BAGE4"
+# report CNV is at least one gene overlaps
+filtered_table <- NULL
+for(i in 1:nrow(input_table)){
+    genes <- input_table[i, "gene_symbol"]
+    if (!is.na(genes)){
+	genes_array <- str_split(genes, "\\|")[[1]]
+	shared_genes <- intersect(genes_array, panel$external_gene_name)
+	if (length(shared_genes) > 0){
+	    filtered_table <- bind_rows(filtered_table, slice(input_table, i))
+	}
+    }
+}
+
 write_excel_csv(filtered_table, args[3])
