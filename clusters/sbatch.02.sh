854d64c27f567cce6efe4cdd2afe67b93a783a2d
naumenko.sa <evolgenomicslab@gmail.com>
2019-11-10 22:05:06 -0500

    modified:   clusters/sbatch.02.sh

--- clusters/sbatch.02.sh
+++ clusters/sbatch.02.sh
@@ -4,7 +4,7 @@
 # https://wiki.rc.hms.harvard.edu/display/O2
 
 #SBATCH --partition=priority        # Partition (queue) priority
-#SBATCH --time=2-00:00              # Runtime in D-HH:MM format
+#SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=project          # Job name
 #SBATCH -c 5			    # cores
 #SBATCH --mem=50G           # Memory needed per CPU or --mem-per-cpu
@@ -13,6 +13,7 @@
 #SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
 
 # render markdown
+# conda activate r is not working on O2 nodes
 # source activate r
 # Rscript --vanilla -e 'rmarkdown::render("03.clustering_new.Rmd")'
 # source deactivate
