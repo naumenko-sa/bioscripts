f41bb5fa99354d35c58b8753afc7111d2807580d
naumenko.sa <evolgenomicslab@gmail.com>
2019-12-23 18:07:21 -0500

    sync

---
+++ scripts/gzip.slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://slurm.schedmd.com/sbatch.html
+# https://wiki.rc.hms.harvard.edu/display/O2
+
+#SBATCH --partition=short        # Partition (queue) priority
+#SBATCH --time=10:00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --job-name=gzip          # Job name
+#SBATCH -c 1			    # cores
+#SBATCH --mem=10G           # Memory needed per CPU or --mem-per-cpu
+#SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
+#SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=FAIL             # Type of email notification (BEGIN, END, FAIL, ALL)
+
+gzip $1
