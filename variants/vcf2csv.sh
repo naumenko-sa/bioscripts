a10b57a1f9a77a0eb051968d0d856172fa5f1dc6
naumenko.sa <evolgenomicslab@gmail.com>
2020-01-20 10:23:14 -0500

    sync

--- variants/vcf2csv.sh
+++ variants/vcf2csv.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 gatk VariantsToTable \
--R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa \
 -V $1 \
 -O $1.tsv \
--F CHROM -F POS -F REF -F ALT -F CSQ \
--U ALLOW_SEQ_DICT_INCOMPATIBILITY
+-F CHROM -F POS -F REF -F ALT -F DP -GF AD -GF AF -F CSQ
