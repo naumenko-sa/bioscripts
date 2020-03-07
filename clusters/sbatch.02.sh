be6f88b8fccd7c0b303462be093f25dbda0e20bf
naumenko.sa <evolgenomicslab@gmail.com>
2020-03-07 17:58:06 -0500

    sync

--- clusters/sbatch.02.sh
+++ clusters/sbatch.02.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=priority        # Partition (queue) priority
 #SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=project          # Job name
-#SBATCH -c 5			    # cores
+#SBATCH -c 7			    # cores
 #SBATCH --mem=50G           # Memory needed per CPU or --mem-per-cpu
 #SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
 #SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
@@ -19,5 +19,7 @@ date
 # source activate r
 # Rscript --vanilla -e 'rmarkdown::render("03.clustering_new.Rmd")'
 # source deactivate
+
+bcbio_nextgen.py ../config/bcbio.yaml -n 7
 
 date
