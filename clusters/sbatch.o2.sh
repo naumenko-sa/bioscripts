0a2f5f04ca4b44a220c7a4f5b752494dba96a42d
naumenko.sa <evolgenomicslab@gmail.com>
2020-06-16 22:37:10 -0400

    sync

--- clusters/sbatch.o2.sh
+++ clusters/sbatch.o2.sh
@@ -4,10 +4,10 @@
 # https://wiki.rc.hms.harvard.edu/display/O2
 
 #SBATCH --partition=priority        # Partition (queue) priority
-#SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --time=30-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=project          # Job name
-#SBATCH -c 7			    # cores
-#SBATCH --mem=50G           # Memory needed per CPU or --mem-per-cpu
+#SBATCH -c 20			    # cores
+#SBATCH --mem=100G                  # total Memory or use --mem-per-cpu
 #SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
 #SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
 #SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
@@ -20,6 +20,6 @@ date
 # Rscript --vanilla -e 'rmarkdown::render("03.clustering_new.Rmd")'
 # source deactivate
 
-bcbio_nextgen.py ../config/bcbio.yaml -n 7
+bcbio_nextgen.py ../config/bcbio.yaml -n 20
 
 date
