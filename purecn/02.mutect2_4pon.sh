78afc0794dbb235f7841b07278cb625651a5423f
naumenko-sa <evolgenomicslab@gmail.com>
2020-06-27 03:27:59 +0000

    sync

--- purecn/02.mutect2_4pon.sh
+++ purecn/02.mutect2_4pon.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # run Mutect2 in T only mode to produce calls for PON
+# or TO calls without matched normals
+
 # $1 = sample_N.bam
 # #2 = panel.interval_list
 
@@ -11,11 +13,11 @@ bname=`basename $1 .bam`
 gatk Mutect2 \
 -R /data/bcbio/genomes/Hsapiens/hg38/seq/hg38.fa \
 -I $1 \
--O $bname.for_pon.vcf.gz \
+-O $bname.vcf.gz \
 --max-mnp-distance 0 \
 --intervals $2 \
 --interval-padding 50 \
 --germline-resource af-only-gnomad.hg38.vcf.gz \
 --genotype-germline-sites
 
-tabix -f $bname.for_pon.vcf.gz
+tabix -f $bname.vcf.gz
