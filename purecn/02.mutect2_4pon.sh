8e86cdaba253b239e226777964840c62c077d517
naumenko-sa <evolgenomicslab@gmail.com>
2020-06-27 03:24:31 +0000

    sync

--- purecn/02.mutect2_4pon.sh
+++ purecn/02.mutect2_4pon.sh
@@ -4,13 +4,14 @@
 # $1 = sample_N.bam
 # #2 = panel.interval_list
 
+# -tumor is deprecated
+
 bname=`basename $1 .bam`
 
 gatk Mutect2 \
 -R /data/bcbio/genomes/Hsapiens/hg38/seq/hg38.fa \
 -I $1 \
 -O $bname.for_pon.vcf.gz \
--tumor S1_N \
 --max-mnp-distance 0 \
 --intervals $2 \
 --interval-padding 50 \
