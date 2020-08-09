7772fa7920d936cf376a851d56cebba32df78036
naumenko.sa <evolgenomicslab@gmail.com>
2020-08-08 22:49:55 -0400

    sync

--- gatkcnv/04.create_pon.sh
+++ gatkcnv/04.create_pon.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-#SBATCH --job-name=bcbio
-#SBATCH --mem=10G
-#SBATCH --export=ALL
-#SBATCH -t 7-50:00
-#SBATCH -p core -n 1
+#SBATCH --partition=priority        # Partition (queue)
+#SBATCH --time=5:00:00              # Runtime in D-HH:MM format
+#SBATCH --job-name=bcbio           # Job name
+#SBATCH -c 1			    # cores
+#SBATCH --mem=10G                   # Memory needed per CPU or --mem
+#SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
+#SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=NONE             # Type of email notification (BEGIN, END, FAIL, ALL)
 
 # gathers all hdf5 files in a PON
 # $1 = panel.gcannotated.tsv
 
+. .profile
+
 hdf5_files=""
-for f in *.hdf5
+for f in `cat hdf5.list`
 do
     hdf5_files="$hdf5_files -I $f"
 done
 
 unset JAVA_HOME && \
-export PATH=/bcbio/anaconda/bin:"$PATH" && \
 gatk --java-options '-Xms500m -Xmx131859m -XX:+UseSerialGC -Djava.io.tmpdir=.' \
 CreateReadCountPanelOfNormals \
 -O cnv.pon.hdf5 \
