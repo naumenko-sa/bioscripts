86e5727939314ebdd350ce55f6ee6bd5d8d7c3b5
naumenko.sa <evolgenomicslab@gmail.com>
2020-08-14 17:32:56 -0400

    sync

--- purecn/41.combine_variants.sh
+++ purecn/41.combine_variants.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=purecn
-#SBATCH --mem=20G
-#SBATCH --export=ALL
-#SBATCH -t 7-50:00
-#SBATCH -p core -n 1
+#SBATCH --partition=priority       # Partition (queue) priority
+#SBATCH --time=10:00:00            # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --job-name=purecn          # Job name
+#SBATCH -c 1                       # cores
+#SBATCH --mem=50G                  # Memory needed per CPU or --mem-per-cpu
+#SBATCH --output=project_%j.out    # File to which STDOUT will be written, including job ID
+#SBATCH --error=project_%j.err     # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=NONE           # Type of email notification (BEGIN, END, FAIL, ALL)
+
+
+# SBATCH --job-name=purecn
+# SBATCH --mem=20G
+# SBATCH --export=ALL
+# SBATCH -t 7-50:00
+# SBATCH -p core -n 1
 
 # $1 = normals.list of vcf.gz
 
