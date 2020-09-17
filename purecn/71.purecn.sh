07d99a2802fad6bfe9e85ccfeec59268a3ae5766
naumenko.sa <evolgenomicslab@gmail.com>
2020-09-17 15:59:29 -0400

    sync

--- purecn/71.purecn.sh
+++ purecn/71.purecn.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 
-# #SBATCH --partition=short          # Partition (queue) priority
-# #SBATCH --time=10:00:00            # Runtime in D-HH:MM format, 10:00:00 for hours
-# #SBATCH --job-name=purecn          # Job name
-# #SBATCH -c 1                       # cores
-# #SBATCH --mem=20G                  # Memory needed per CPU or --mem-per-cpu
-# #SBATCH --output=project_%j.out    # File to which STDOUT will be written, including job ID
-# #SBATCH --error=project_%j.err     # File to which STDERR will be written, including job ID
-# #SBATCH --mail-type=NONE           # Type of email notification (BEGIN, END, FAIL, ALL)
+#SBATCH --partition=short          # Partition (queue) priority
+#SBATCH --time=10:00:00            # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --job-name=purecn          # Job name
+#SBATCH -c 10                       # cores
+#SBATCH --mem=30G                  # Memory needed per CPU or --mem-per-cpu
+#SBATCH --output=project_%j.out    # File to which STDOUT will be written, including job ID
+#SBATCH --error=project_%j.err     # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=NONE           # Type of email notification (BEGIN, END, FAIL, ALL)
 
 
-#SBATCH --job-name=purecn
-#SBATCH --mem=30G
-#SBATCH --export=ALL
-#SBATCH -t 7-50:00
-#SBATCH -p core -n 10
+# #SBATCH --job-name=purecn
+# #SBATCH --mem=30G
+# #SBATCH --export=ALL
+# #SBATCH -t 7-50:00
+# #SBATCH -p core -n 10
 
 
 # once OOM killed with 20G and bootstrap500
