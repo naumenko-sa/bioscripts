1b936878a7128eaccdfdf8548fbe28d2c5614597
naumenko.sa <evolgenomicslab@gmail.com>
2020-11-05 16:51:30 -0500

    sync

--- scripts/bcl2fq.sh
+++ scripts/bcl2fq.sh
@@ -7,21 +7,24 @@
 #SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=bcl2fq          # Job name
 #SBATCH -c 20			    # cores
-#SBATCH --mem=50G           # Memory needed per CPU or --mem-per-cpu
+#SBATCH --mem=25G           # Memory needed per CPU or --mem-per-cpu
 #SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
 #SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
 #SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
 
 date
 
-module load bcl2fastq/2.18.0.12
+#module load bcl2fastq/2.18.0.12
+module load bcl2fastq/2.20.0.422
 
 # Harvard Single Cell core script - no trimming of reads
 # we use bcl2fastq version 2.18.0.12
 bcl2fastq --use-bases-mask y*,y*,y*,y* \
 --mask-short-adapter-reads 0 \
 --minimum-trimmed-read-length 0 \
---processing-threads 20
+--processing-threads 20 \
+--sample-sheet $1 \
+--no-lane-splitting
 
 # Bauer core 
 # bcl2fastq \
