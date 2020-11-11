b4ac1d5e9c32fb77bd6f063132343db60c654d0c
naumenko-sa <evolgenomicslab@gmail.com>
2020-11-11 18:48:36 +0000

    sync

--- bam/bam.sort.sh
+++ bam/bam.sort.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/bin/bash -l
+
+#SBATCH --job-name=samtools
+#SBATCH --mem=8G
+#SBATCH --export=ALL
+#SBATCH -t 7-50:00
+#SBATCH -p core -n 1
 
 # sam2bam
-#samtools view -bS $1 > $bamname
+# samtools view -bS $1 > $bamname
 
 sortedbam=`echo $1 | sed s/bam/sorted.bam/`
 samtools sort $bam $sortedbam
