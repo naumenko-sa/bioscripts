58ef092431573c4a76c513a9c82593dc43e8e593
naumenko-sa <evolgenomicslab@gmail.com>
2020-11-11 15:44:13 +0000

    sync

---
+++ quality_control/preseq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=preseq
+#SBATCH --mem=8G
+#SBATCH --export=ALL
+#SBATCH -t 7-50:00
+#SBATCH -p core -n 1
+
+
+# http://smithlabresearch.org/software/preseq/
+
+# $1 = file.bam
+
+bname=`basename $1 .bam`
+
+preseq c_curve -o $bname.out $1
