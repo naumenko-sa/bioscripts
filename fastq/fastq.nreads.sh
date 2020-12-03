1673a5516704b29e017781af599f29c2e3920bb8
naumenko.sa <evolgenomicslab@gmail.com>
2020-12-02 21:37:28 -0500

    sync

--- fastq/fastq.nreads.sh
+++ fastq/fastq.nreads.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-#PBS -l walltime=2:00:00,nodes=1:ppn=1
-#PBS -joe .
-#PBS -d .
-#PBS -l vmem=10g,mem=10g
+#SBATCH --partition=priority        # Partition (queue) priority
+#SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --job-name=qc               # Job name
+#SBATCH -c 1			    # cores
+#SBATCH --mem=10G                   # Memory needed per CPU or --mem-per-cpu
+#SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
+#SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
+#SBATCH --mail-type=NONE             # Type of email notification (BEGIN, END, FAIL, ALL)
+
 
 zcat $1 | wc -l | awk '{print $0/4}' > $1.nreads
