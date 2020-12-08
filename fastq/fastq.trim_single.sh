45d51d9230ad9f4999da355ec9aa6860b709ce68
naumenko.sa <evolgenomicslab@gmail.com>
2020-12-08 18:57:30 -0500

    sync

--- fastq/fastq.trim_single.sh
+++ fastq/fastq.trim_single.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --partition=priority        # Partition (queue) priority
 #SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
-#SBATCH --job-name=qc               # Job name
+#SBATCH --job-name=trim_single      # Job name
 #SBATCH -c 10			    # cores
 #SBATCH --mem=20G                   # Memory needed per CPU or --mem-per-cpu
 #SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
