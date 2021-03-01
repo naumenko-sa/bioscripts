70389791ab49c9d721e5da8bee1062214fc774b9
naumenko.sa <evolgenomicslab@gmail.com>
2021-03-01 15:48:14 -0500

    sync

--- purecn/71.purecn.sh
+++ purecn/71.purecn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
-#SBATCH --partition=short          # Partition (queue) priority
+#SBATCH --partition=priority       # Partition (queue) priority
 #SBATCH --time=10:00:00            # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=purecn          # Job name
-#SBATCH -c 10                       # cores
-#SBATCH --mem=30G                  # Memory needed per CPU or --mem-per-cpu
+#SBATCH -c 10                      # cores
+#SBATCH --mem=50G                  # Memory needed per CPU or --mem-per-cpu
 #SBATCH --output=project_%j.out    # File to which STDOUT will be written, including job ID
 #SBATCH --error=project_%j.err     # File to which STDERR will be written, including job ID
 #SBATCH --mail-type=NONE           # Type of email notification (BEGIN, END, FAIL, ALL)
@@ -17,7 +17,7 @@
 # #SBATCH -p core -n 10
 
 
-# once OOM killed with 20G and bootstrap500
+# once OOM killed with 30G and bootstrap500
 date
 
 . .profile
@@ -26,7 +26,7 @@ which Rscript
 # $1 = tumor.coverage_loess.txt.gz
 # $2 = mutect.filtered.vcf.gz
 
-SAMPLEID=`echo $2 | awk -F '.' '{print $1}'`
+SAMPLEID=`echo $2 | awk -F '-' '{print $1}'`
 
 echo $SAMPLEID
 
--- scripts/tar.sh
+++ scripts/tar.sh
@@ -3,16 +3,20 @@
 # https://slurm.schedmd.com/sbatch.html
 # https://wiki.rc.hms.harvard.edu/display/O2
 
-#SBATCH --partition=short        # Partition (queue) priority
-#SBATCH --time=12:00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
-#SBATCH --job-name=tart          # Job name
+#SBATCH --partition=priority         # Partition (queue) priority
+#SBATCH --time=5-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
+#SBATCH --job-name=tar          # Job name
 #SBATCH -c 1			    # cores
-#SBATCH --mem=10G                  # total Memory or use --mem-per-cpu
+#SBATCH --mem=20G                  # total Memory or use --mem-per-cpu
 #SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
 #SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
 #SBATCH --mail-type=NONE             # Type of email notification (BEGIN, END, FAIL, ALL)
 
+# $1 = dir_to_archive
+
 date
-tar xzf $1
+tar czf $1.tar.gz $1
+md5 $1.tar.gz > $1.tar.gz.md5
 date
+
 #tar cf $file.tar $file
