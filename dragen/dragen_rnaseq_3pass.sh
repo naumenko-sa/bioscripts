22e6edd138e22f6e337cd98c16a8cdef61e8bb9d
naumenko-sa <evolgenomicslab@gmail.com>
2021-10-05 14:00:13 +0000

    update dragen 3 pass

--- dragen/dragen_rnaseq_3pass.sh
+++ dragen/dragen_rnaseq_3pass.sh
@@ -14,24 +14,24 @@ sample=$1
 
 mkdir -p dragen_${sample}/pass1
 
-project_dir=/staging/2021-10-03_fusions_3pass
+project_dir=/staging/2021-10-04_rnaseq_fusions
 
 # pass 1
 if [ ! -f ${project_dir}/dragen_${sample}/pass1/$sample.SJ.out.tab ]; then
 
 dragen \
---output-directory /staging/2021-10-03_fusions_3pass/dragen_${sample}/pass1 \
+--output-directory ${project_dir}/dragen_${sample}/pass1 \
 -r /staging/reference/genomes/Hsapiens/hg19 \
 --output-file-prefix $sample \
--1 /staging/2021-10-03_fusions_3pass/input/${sample}_1.fq.gz \
--2 /staging/2021-10-03_fusions_3pass/input/${sample}_2.fq.gz \
+-1 ${project_dir}/input/${sample}_1.fq.gz \
+-2 ${project_dir}/input/${sample}_2.fq.gz \
 --enable-rna true \
 --annotation-file /staging/reference/genomes/Hsapiens/hg19/ref-transcripts.gtf \
---enable-rna-quantification false \
+--enable-rna-quantification true \
 --RGID $sample \
 --RGSM $sample \
 --intermediate-results-dir /staging/tmp \
---enable-rna-gene-fusion false
+--enable-rna-gene-fusion true
 
 fi
 
@@ -42,13 +42,13 @@ mkdir -p dragen_${sample}/pass2
 if [ ! -f ${project_dir}/dragen_${sample}/pass2/$sample.bam ]; then
 
 dragen \
---output-directory /staging/2021-10-03_fusions_3pass/dragen_${sample}/pass2 \
+--output-directory ${project_dir}/dragen_${sample}/pass2 \
 -r /staging/reference/genomes/Hsapiens/hg19 \
 --output-file-prefix $sample \
--1 /staging/2021-10-03_fusions_3pass/input/${sample}_1.fq.gz \
--2 /staging/2021-10-03_fusions_3pass/input/${sample}_2.fq.gz \
+-1 ${project_dir}/input/${sample}_1.fq.gz \
+-2 ${project_dir}/input/${sample}_2.fq.gz \
 --enable-rna true \
---annotation-file /staging/2021-10-03_fusions_3pass/dragen_${sample}/pass1/$sample.SJ.out.tab \
+--annotation-file ${project_dir}/dragen_${sample}/pass1/$sample.SJ.out.tab \
 --enable-rna-quantification false \
 --RGID $sample \
 --RGSM $sample \
@@ -63,7 +63,7 @@ fi
 mkdir -p dragen_${sample}/pass3
 
 dragen \
---output-directory /staging/2021-10-03_fusions_3pass/dragen_${sample}/pass3 \
+--output-directory ${project_dir}/dragen_${sample}/pass3 \
 -r /staging/reference/genomes/Hsapiens/hg19 \
 --output-file-prefix $sample \
 -b ${project_dir}/dragen_${sample}/pass2/$sample.bam \
