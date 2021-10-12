0ee25468647bebf41f4e260053f0ce7f0f9bcfa5
naumenko-sa <evolgenomicslab@gmail.com>
2021-10-12 15:09:52 +0000

    add gather stats

--- fusions/arriba.sh
+++ fusions/arriba.sh
@@ -20,6 +20,6 @@ arriba \
 -o fusions.tsv \
 -O fusions.discarded.tsv \
 -T -P -i chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY \
--b ${reference}/rnaseq/fusion-blacklist/arriba-blacklist.tsv.gz 
+-b ${reference}/rnaseq/fusion-blacklist/arriba-blacklist.tsv.gz
 
 date
