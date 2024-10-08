018dc58d3b9147be82d72f9f29ccff62434732c9
Serhiy Naumenko <evolgenomicslab@gmail.com>
2024-10-08 13:38:08 -0400

    update vep call to handle indels

--- vep/01.table2vep.sh
+++ vep/01.table2vep.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # convert EMG exported table to VEP input
-# the first line also removed the header 
 cat EMG\ output\ Sept\ 2024.csv | cut -d, -f 1-4,6 | grep -v "<p." | grep -v "<div" | grep "NM_" | sed s/","/"\t"/g > variants.tsv
-cat variants.tsv | awk -F '\t' '{print $1"\t"$2"\t"$2"\t"$3"/"$4"\t+\tNSOVAR"NR}' > variants.vep_input.txt
+# produce tab output (not good for indels)
+#cat variants.tsv | awk -F '\t' '{print $1"\t"$2"\t"$2"\t"$3"/"$4"\t+\tNSOVAR"NR}' > variants.vep_input.txt
+
+# produce vcf output - ok for indels
+cat variants.tsv | awk -F '\t' '{print $1"\t"$2"\tNSOVAR"NR"\t"$3"\t"$4"\t.\t.\t."}' > variants.vep_input_vcf.txt
--- vep/03.vep_run.sh
+++ vep/03.vep_run.sh
@@ -3,7 +3,7 @@
 sudo docker run -t -i -v /data/vep_data:/opt/vep/.vep:Z ensemblorg/ensembl-vep vep \
 -o /opt/vep/.vep/variants.annotated.tsv \
 --tab --cache  --merge --force_overwrite --offline --no_stats \
--input_file /opt/vep/.vep/variants.vep_input.txt \
+-input_file /opt/vep/.vep/variants.vep_input_vcf.txt \
 --fasta /opt/vep/.vep/homo_sapiens/112_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz \
 --symbol --numbers --biotype --total_length --canonical --hgvs --shift_hgvs 1 --af_gnomad \
 --fields "Uploaded_variation,Location,Allele,Gene,Feature,Feature_type,Consequence,cDNA_position,CDS_position,Protein_position,Amino_acids,Codons,Existing_variation,IMPACT,DISTANCE,STRAND,FLAGSSYMBOL,SYMBOL_SOURCE,HGNC_ID,BIOTYPE,CANONICAL,REFSEQ_MATCH,SOURCE,REFSEQ_OFFSET,GIVEN_REF,USED_REF,BAM_EDIT,EXON,INTRON,HGVSc,HGVSp,HGVS_OFFSET,gnomADe_AF,CLIN_SIG"
