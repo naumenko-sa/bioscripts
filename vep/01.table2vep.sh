09a7dc9b3413bfd236dce2ff75886f9e04f37788
Serhiy Naumenko <evolgenomicslab@gmail.com>
2024-10-08 09:29:14 -0400

    update table2vep

--- vep/01.table2vep.sh
+++ vep/01.table2vep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # convert EMG exported table to VEP input
+# the first line also removed the header 
 cat EMG\ output\ Sept\ 2024.csv | cut -d, -f 1-4,6 | grep -v "<p." | grep -v "<div" | grep "NM_" | sed s/","/"\t"/g > variants.tsv
-cat variants.tsv | sed 1d | awk -F '\t' '{print $1"\t"$2"\t"$2"\t"$3"/"$4"\t+\tNSOVAR"NR}' > variants.vep_input.txt
+cat variants.tsv | awk -F '\t' '{print $1"\t"$2"\t"$2"\t"$3"/"$4"\t+\tNSOVAR"NR}' > variants.vep_input.txt
