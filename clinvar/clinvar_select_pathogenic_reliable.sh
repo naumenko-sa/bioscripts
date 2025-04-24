5078f7ffab55cc49ef08112d02bed100cef85f76
Serhiy Naumenko <evolgenomicslab@gmail.com>
2025-04-23 21:44:28 -0400

    fix clinvar filtration

--- clinvar/clinvar_select_pathogenic_reliable.sh
+++ clinvar/clinvar_select_pathogenic_reliable.sh
@@ -2,13 +2,13 @@
 
 # select pathogenic variants reviewed by expert panel or supported by multiple labs
 # install vembrane via conda and activate vembrane env
-# CLNSIG is a tuple for some reason
+# if CLNSIG has a comma, it is parced as a tuple, i.e. "Pathogenic/Pathogenic,_low_penetrance|other" = ("Pathogenic/Pathogenic", "_low_penetrance|other")
 
 # starts with Pathogenic or Likely_pathogetic, checksum matches
 vembrane filter 'any(clnsig[:10]=="Pathogenic" or clnsig[:17]=="Likely_pathogenic" for clnsig in INFO["CLNSIG"])' clinvar.vcf.gz > clinvar.pathogenic.vcf
 
 # this does not work for the bottom 4 and P/LP/P,_low_penetrance
-#vembrane filter 'any(clnsig in INFO["CLNSIG"] for clnsig in ("Pathogenic", "Likely_pathogenic", "Pathogenic/Likely_pathogenic", \
+# vembrane filter 'any(clnsig in INFO["CLNSIG"] for clnsig in ("Pathogenic", "Likely_pathogenic", "Pathogenic/Likely_pathogenic", \
 #                     "Pathogenic|other", "Pathogenic|drug_response", "Likely_pathogenic|drug_response", "Pathogenic|risk_factor", \
 #                     "Likely_pathogenic/Likely_risk_allele", "Pathogenic/Likely_risk_allele", "Likely_pathogenic|risk_factor", \
 #                     "Pathogenic|Affects", "Likely_pathogenic,_low_penetrance", "Pathogenic/Likely_pathogenic|other", "Likely_pathogenic|association", \
@@ -25,7 +25,13 @@ vembrane filter 'any(clnsig[:10]=="Pathogenic" or clnsig[:17]=="Likely_pathogeni
 bgzip clinvar.pathogenic.vcf
 tabix clinvar.pathogenic.vcf.gz
 
-vembrane filter 'any(clnrevstat=="reviewed_by_expert_panel" or clnrevstat=="criteria_provided,_multiple_submitters,_no_conflicts" for clnrevstat in INFO["CLNREVSTAT"])' clinvar.pathogenic.vcf.gz > clinvar.selected.vcf
+# same story with CLNREVSTAT, when there is a comma, it parses as a tuple
+# we need to extract
+# criteria_provided,_multiple_submitters,_no_conflicts
+# reviewed_by_expert_panel
+
+vembrane filter '(INFO["CLNREVSTAT"][0] == "criteria_provided" and INFO["CLNREVSTAT"][1] == "_multiple_submitters" and INFO["CLNREVSTAT"][2] == "_no_conflicts") or \
+                 (INFO["CLNREVSTAT"][0] == "reviewed_by_expert_panel")' clinvar.pathogenic.vcf.gz > clinvar.selected.vcf
 
 bgzip clinvar.selected.vcf
 tabix clinvar.selected.vcf.gz
