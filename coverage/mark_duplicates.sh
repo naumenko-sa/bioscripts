b11d7e08a7f0e328d13d3ec793e92deefa8e67f7
Serhiy Naumenko <evolgenomicslab@gmail.com>
2025-05-09 11:18:03 -0400

    fix mark duplicates

--- coverage/mark_duplicates.sh
+++ coverage/mark_duplicates.sh
@@ -15,11 +15,11 @@ sample="${file%%.*}"
 echo $ext
 
 if [[ "$ext" == "bam" ]];then
-    gatk MarkDuplicates --java-options "-XmX20G" -I $1 -O $sample.marked.bam -M $sample.dup_metrics.txt
+    gatk --java-options "-Xmx20G" MarkDuplicates -I $1 -O $sample.marked.bam -M $sample.dup_metrics.txt
     #--QUIET true --VERBOSITY ERROR
 elif [[ "$ext" == "cram" ]];then
     # output is bam
-    gatk MarkDuplicates --java-options "-Xmx20G" -I $1 -O $sample.marked.bam -M $sample.dup_metrics.txt -R $2
+    gatk --java-options "-Xmx20G" MarkDuplicates -I $1 -O $sample.marked.bam -M $sample.dup_metrics.txt -R $2
     #--QUIET true --VERBOSITY ERROR
 fi
 
