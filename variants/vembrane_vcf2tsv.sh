20f93f85ee2e6fb08ac91d7d9dd4c1ff9d43ce14
Serhiy Naumenko <evolgenomicslab@gmail.com>
2025-07-23 15:17:45 -0400

    update

--- variants/vembrane_vcf2tsv.sh
+++ variants/vembrane_vcf2tsv.sh
@@ -4,4 +4,4 @@
 
 bname=`basename $1 .vcf.gz`
 
-vembrane table --annotation-key CSQ 'INFO["RS"], CHROM, POS, REF, ALT, INFO["ALLELEID"], INFO["CLNDN"], INFO["CLNHGVS"], INFO["GENEINFO"], INFO["CLNVI"]' $1 > $bname.tsv
+vembrane table --annotation-key CSQ 'INFO["RS"], CHROM, POS, REF, ALT, INFO["ALLELEID"], INFO["CLNDN"], INFO["CLNHGVS"], INFO["GENEINFO"], INFO["CLNVI"], INFO["MC"] ' $1 > $bname.tsv
