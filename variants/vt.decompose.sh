3acb77b2dcca2fc77e17452c2590b8d6ea040ab9
Serhiy Naumenko <evolgenomicslab@gmail.com>
2025-07-25 12:46:40 -0400

    update decompose

--- variants/vt.decompose.sh
+++ variants/vt.decompose.sh
@@ -4,6 +4,7 @@
 # https://gemini.readthedocs.io/en/latest/content/preprocessing.html
 
 # $1 = test.vcf.gz
+# $2 = reference.fa
 
 #bgzip -d $1
 bname=`basename $1 .vcf.gz`
@@ -11,6 +12,6 @@ bname=`basename $1 .vcf.gz`
 #already fixed in the new version
 #cat $bname.vcf | sed s/"##FORMAT=<ID=AD,Number=."/"##FORMAT=<ID=AD,Number=R"/ > $bname.modified.vcf
 
-vt decompose -s $1 | vt normalize -r ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -n - | vt uniq - -o $bname.decomposed.vcf.gz
+vt decompose -s $1 | vt normalize -r $2 -n - | vt uniq - -o $bname.decomposed.vcf.gz
 
 tabix $bname.decomposed.vcf.gz
