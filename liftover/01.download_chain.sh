178eadbbeef757334fbfbcc2d7696e65421c2edc
Serhiy Naumenko <evolgenomicslab@gmail.com>
2025-09-30 10:10:44 -0400

    add liftover

---
+++ liftover/01.download_chain.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -c https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
---
+++ liftover/03.lift_over_cfvcf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $1 - input.vcf (not gz)
+# $2 - hg19.fa
+# $3 - hg38.fa
+# $4 - chain.gz
+
+bname=`basename $1 .vcf`
+
+#bcftools norm -m -both -f $2 $1 | bgzip -c > $bname.norm.vcf.gz
+#tabix -p vcf $bname.vcf.gz
+
+gatk LiftoverVcf \
+  I=$1 \
+  O=$bname.lifted.vcf \
+  CHAIN=$4 \
+  REJECT=$bname.rejected.vcf \
+  R=$3
+
+#bcftools norm -f $3 -m -both $bname.lifted.vcf.gz | bgzip -c > $bname.lifted.norm.vcf.gz
+#tabix -p vcf $bname.lifted.norm.vcf.gz
