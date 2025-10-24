d42aed8f8483616cf2f373fe3c1c0491f9a2f59d
Serhiy Naumenko <evolgenomicslab@gmail.com>
2025-10-24 09:19:28 -0400

    update

--- basespace/bcl2fastq.sh
+++ basespace/bcl2fastq.sh
@@ -5,3 +5,8 @@
 
 # NovaSeqX requires bcl-convert
 # rpm2cpio bcl-convert-4.1.7-2.el7.x86_64.rpm | cpio -idmv
+
+# needs sudo for some reason to write files
+# input dir is a run dir
+# to alter sample sheet it has to be copied
+# sudo /data/tools/bcl-convert --bcl-input-directory $1 --output-directory ./fastq --sample-sheet ./SampleSheet.csv &> log.txt
