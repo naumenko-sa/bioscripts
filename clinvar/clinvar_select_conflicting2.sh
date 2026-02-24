500172bedd4c3e8530715101f57ea8b0bea778c7
Serhiy Naumenko <evolgenomicslab@gmail.com>
2026-02-24 14:31:17 -0500

    update

--- clinvar/clinvar_select_conflicting2.sh
+++ clinvar/clinvar_select_conflicting2.sh
@@ -2,7 +2,9 @@
 
 # select by AF > 0.001 after annotation - pathogenic variants that missed by freq arm
 bname=`basename $1 .vcf.gz`
-vembrane filter --annotation-key CSQ 'INFO["max_af_regeneron"] >= 0.001 or CSQ["AFR_AF"] >= 0.001 or CSQ["AMR_AF"] >= 0.001' > $bname.high_af_path.vcf
+bcftools +fill-tags $1 -Oz -o $bname.max.vcf.gz -- -t 'max_af_calc:Float=fmax(max_af_regeneron|0, fmax(AFR_AF|0, AMR_AF|0))'
+tabix $bname.max.vcf.gz
+vembrane filter --annotation-key CSQ 'INFO["max_af_calc"] >= 0.001' $bname.max.vcf.gz > $bname.high_af_path.vcf
 #  EAS_AF, EUR_AF, SAS_AF,
 #                                                                gnomADe_AFR_AF, gnomADe_AMR_AF, gnomADe_EAS_AF,
 #                                                                gnomADe_NFE_AF, gnomADe_SAS_AF, gnomADg_AFR_AF,
